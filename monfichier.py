9c83d407cecd14b6fcb0be081f8677605f153c35
Haagard <haagard8@gmail.com>
2024-11-05 15:26:56 +0100

    langue

--- monfichier.py
+++ monfichier.py
@@ -1,4 +1,4 @@
 first_name = input("Prénom :")
 last_name = input("Nom :")
 age = input("Age :")
-print(f"Bonjour",first_name,last_name,age,"years old")
+print(f"Hello",first_name,last_name,age,"years old")
